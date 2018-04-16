fa2cff4b628ff54b64d293d92b3a646a9dd27369
Winfried Baumann <codewing@web.de>
2018-04-16 20:30:35 +0200

    upgraded glm due to compiler recognition errors in linux in version 0.9.8

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,6 @@
 
 int main() {
 
-    glm::mat4 matrix;
-    glm::vec4 vector;
-    glm::vec4 result = matrix * vector;
-
     try {
         std::shared_ptr<Renderer> renderer = std::make_shared<Renderer>(800, 600);
         while(renderer->Run()){
