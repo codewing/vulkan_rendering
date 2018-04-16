90d39948a0751ee06d1d5af1833bb5cc3508e903
Winfried Baumann <codewing@web.de>
2018-04-16 20:01:51 +0200

    added glm with a glm test in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <memory>
 #include "Renderer.h"
+#include <glm/glm.hpp>
 
 int main() {
+
+    glm::mat4 matrix;
+    glm::vec4 vector;
+    glm::vec4 result = matrix * vector;
 
     try {
         std::shared_ptr<Renderer> renderer = std::make_shared<Renderer>(800, 600);
