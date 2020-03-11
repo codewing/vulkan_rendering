97bea37703db8d6fab3cabea4124d02fdc0e0966
codewing <codewing@web.de>
2020-03-12 00:02:50 +0100

    moved variables out of try catch

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@
 
 int main() {
 
+    std::unique_ptr<Time> time = std::make_unique<Time>();
+    std::shared_ptr<Scene> scene = std::make_shared<Scene>();
+    std::shared_ptr<GLFWWindow> window = std::make_shared<GLFWWindow>("Vulkan Window", 1024, 768);
+    std::shared_ptr<Renderer> renderer = std::make_shared<Renderer>(scene, std::static_pointer_cast<Window>(window));
+
     try {
-        std::unique_ptr<Time> time = std::make_unique<Time>();
-        std::shared_ptr<Scene> scene = std::make_shared<Scene>();
-        std::shared_ptr<GLFWWindow> window = std::make_shared<GLFWWindow>("Vulkan Window", 1024, 768);
-        std::shared_ptr<Renderer> renderer = std::make_shared<Renderer>(scene, std::static_pointer_cast<Window>(window));
         do {
             time->UpdateTime();
 
